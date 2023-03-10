2869ea808533f8c38f84a1cc13aeb79c0c843e3f
Simba <isarawadano1@gmail.com>
2023-03-10 15:38:10 +0200

    comments

--- Tutorial/tutorial_1.php
+++ Tutorial/tutorial_1.php
@@ -2,4 +2,14 @@
 
     #Syntax
     echo "Hello World";
+    
+    #Comments
+
+    // Single line comment variation 1
+
+    #Single line comment variation 2
+    
+    /* Multiline commment
+     are like this */
+
 ?>
